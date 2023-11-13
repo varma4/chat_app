6891a73d3461a15ba1341b4d9fe3bd7328f13cd0
Varma <mrvarma85@gmail.com>
2023-11-14 02:52:18 +0530

    showing rooms

--- index.js
+++ index.js
@@ -1,39 +1,44 @@
-const express = require(`express`)
-const http = require('http')
-const socketIO = require("socket.io")
+
+
+
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 const cors = require('cors');
-const app = express()
-const server = http.createServer(app)
-const io = socketIO(server)
-app.use(cors())
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+app.use(cors());
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-//io is for all the clients
-io.on('connection', (socket) =>{
-    socket.on('joinRoom', (room, userName) => {
-        socket.join(room)
-        // socket.emit('message', room)
-        // io.to(room).emit('message', `${userName}  has joined`)
-        io.to(room).emit('message', {userName: userName, message: "joined"})
-    })
+const activeRooms = new Set();
 
-    socket.on('sendMessage', (data) => {
-        io.to(data.room).emit('message', { userName: data.userName, message: data.message })
-    })
+function updateRoomList(socket) {
+    socket.emit('updateRoomList', [...activeRooms]);
+  }
+  
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
-    
+io.on('connection', (socket) => {
+  // Send the current list of active rooms to the newly connected user
+  updateRoomList(socket);
 
-} )
+  socket.on('joinRoom', (room, userName) => {
+    socket.join(room);
+    activeRooms.add(room);
+    io.to(room).emit('message', { userName: 'System', message: `${userName} joined the room` });
 
+    updateRoomList(socket);
+  });
 
+  socket.on('sendMessage', (data) => {
+    io.to(data.room).emit('message', { userName: data.userName, message: data.message });
+  });
 
-const port = 3000
+});
+
+const port = 3000;
 
 server.listen(port, () => {
-    console.log(`up and running on port ${port}`);
-})
-
+  console.log(`Server is running on port ${port}`);
+});
--- public/app.js
+++ public/app.js
@@ -1,40 +1,77 @@
-const socket = io()
 
 
-let roomName = ""
-let userName = ""
+const socket = io();
+let roomName = "";
+let userName = "";
 
-function createRoom() 
-{
-    roomName = document.getElementById("room_name").value.toLowerCase()
-    userName = document.getElementById("user_name").value.toLowerCase()
-    console.log(roomName, userName);
 
-    if (roomName.trim() !== '' && userName !== '') {
-        document.getElementById('detailsRoom').style.display = 'none';
-        document.getElementById('chat-room').style.display = 'flex';
-        socket.emit('joinRoom', roomName, userName);
-    }
+const storedRooms = JSON.parse(sessionStorage.getItem("storedRooms")) || [];
+
+// Function to update the room list
+function updateRoomList(rooms) {
+  const roomList = document.getElementById("room-list");
+  roomList.innerHTML = "";
+
+  rooms.forEach((room) => {
+    const li = document.createElement("li");
+    li.style.listStyle = "none"
+    li.style.background = "lightblue"
+    li.style.padding = "5px"
+    li.style.borderRadius = "10px"
+    li.style.textAlign= "center"
+    li.appendChild(document.createTextNode(room));
+    li.addEventListener("click", () => {
+      document.getElementById("room_name").value = room;
+    });
+    roomList.appendChild(li);
+  });
 }
 
-function sendMessage() 
-{
-    const message = document.getElementById("message_input").value
-    if (message !== '') {
-        socket.emit('sendMessage', { room: roomName, userName, message });
-        document.getElementById('message_input').value = '';
-    }
-}
 
-function showMessage(message) 
-{
-    const ul = document.getElementById("messages")
-    const li = document.createElement('li')
-    li.className = message.userName === userName ? 'right-message' : 'left-message';
-    li.appendChild(document.createTextNode(`${message.userName}: ${message.message}`));
-    ul.appendChild(li)
-}
 
-socket.on('message', (message) => {
-    showMessage(message);
+
+
+socket.on("updateRoomList", (rooms) => {
+  updateRoomList(rooms);
 });
+
+function createRoom() {
+  roomName = document.getElementById("room_name").value.toLowerCase();
+  userName = document.getElementById("user_name").value.toLowerCase();
+  console.log(roomName, userName);
+
+  if (roomName.trim() !== "" && userName !== "") {
+    storedRooms.push(roomName);
+    sessionStorage.setItem("storedRooms", JSON.stringify(storedRooms));
+
+    document.getElementById("detailsRoom").style.display = "none";
+    document.getElementById("chat-room").style.display = "flex";
+    document.getElementById('active-rooms').style.display = "none";
+    socket.emit("joinRoom", roomName, userName);
+  }
+}
+
+function sendMessage() {
+  const message = document.getElementById("message_input").value;
+  if (message !== "") {
+    socket.emit("sendMessage", { room: roomName, userName, message });
+    document.getElementById("message_input").value = "";
+  }
+}
+
+function showMessage(message) {
+  const ul = document.getElementById("messages");
+  const li = document.createElement("li");
+  li.className =
+    message.userName === userName ? "right-message" : "left-message";
+  li.appendChild(
+    document.createTextNode(`${message.userName}: ${message.message}`)
+  );
+  ul.appendChild(li);
+  ul.scrollTop = ul.scrollHeight;
+}
+
+socket.on("message", (message) => {
+  showMessage(message);
+});
+
